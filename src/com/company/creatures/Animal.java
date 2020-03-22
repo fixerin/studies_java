4fcdddc22ceb16cb0bcf0c6d67aa8cd97aa531c7
fixerin <jan.malinski94@gmail.com>
2020-03-22 21:02:58 +0100

    v 0.4, dodanie toString, dodanie paczek

--- src/com/company/creatures/Animal.java
+++ src/com/company/creatures/Animal.java
@@ -3,24 +3,28 @@ package com.company.creatures;
 import java.io.File;
 
 public class Animal {
-    public final String species;
+    private final String species;
     private Double weight;
     public String name;
     public File pic;
 
-    public static final Double DEFAULT_DOG_WEIGHT = 10.0;
-    public static final Double DEFAULT_MOUSE_WEIGHT = 0.12;
-    public static final Double DEFAULT_LION_WEIGHT = 23.0;
+    private static final Double DEFAULT_DOG_WEIGHT = 10.0;
+    private static final Double DEFAULT_MOUSE_WEIGHT = 0.12;
+    private static final Double DEFAULT_LION_WEIGHT = 23.0;
 
 
     public Animal(String species) {
         this.species = species;
-        if (species.equals("dog")) {
-            this.weight = DEFAULT_DOG_WEIGHT;
-        } else if (species.equals("mouse")) {
-            this.weight = DEFAULT_MOUSE_WEIGHT;
-        } else if (species.equals("lion")) {
-            this.weight = DEFAULT_LION_WEIGHT;
+        switch (species) {
+            case "dog":
+                this.weight = DEFAULT_DOG_WEIGHT;
+                break;
+            case "mouse":
+                this.weight = DEFAULT_MOUSE_WEIGHT;
+                break;
+            case "lion":
+                this.weight = DEFAULT_LION_WEIGHT;
+                break;
         }
     }
 
@@ -45,7 +49,7 @@ public class Animal {
             System.out.println("You can't walk with dead animal.");
         }
     }
-    public boolean alive(){
+    private boolean alive(){
         if (weight > 0) {
             return true;
         } else {
